09ba8090eea77e74c7a2cb1c0edd586b4f16e844
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-07-31 13:35:01 +0430

    Fix something and Import banner in the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import NavBar from "./components/navbar/NavBar";
+import Banner from "./components/banner/Banner";
 
 function App() {
   return (
     <>
       <NavBar />
+      <Banner />
     </>
   );
 }
